8837c5904b112bdbb2e6b5333e6f1ccba2104d6c
chenxiang <baoluo.cx@alibaba-inc.com>
2021-03-11 21:03:14 +0800

    add files

--- tensorflow/recommendation/fm_estimator.py
+++ tensorflow/recommendation/fm_estimator.py
@@ -1,84 +1,73 @@
 import tensorflow as tf
 from common_path import *
 from fm_regression import RatingDataset
+from layers import *
 
 
-class RegressorNet:
-    def __init__(self, config):
-        self.config = config
-        self.model = None
-        self.variable_dict = dict()
-        self.initialize()
+def create_model(features, feature_columns, hidden_units, output_cls):
+    inputs = tf.feature_column.input_layer(features=features, feature_columns=feature_columns)
+    for unit in hidden_units:
+        inputs = tf.layers.dense(inputs, unit, activation=tf.nn.relu)
+    logits = tf.layers.dense(inputs, output_cls, activation=tf.nn.sigmoid)
+    return logits
 
-    def initialize(self):
-        for name in self.config.keys():
-            params = self.config[name]
-            variable = tf.get_variable(name, shape=[params['buckets'], params['dim']], dtype=tf.float32,
-                                       initializer=tf.truncated_normal_initializer(stddev=0.002))
-            self.variable_dict[name] = variable
-
-    def get_embedding(self, feature_dict):
-        embedding_list = list()
-        for colname in self.config.keys():
-            params = self.config[colname]
-            if params['type'] == 'string':
-                indices = tf.strings.to_hash_bucket_fast(feature_dict[colname], params['buckets'])
-                embedding = tf.nn.embedding_lookup(self.variable_dict[colname], indices)
-            else:
-                embedding = tf.nn.embedding_lookup(feature_dict[colname], sample_dict[colname])
-            embedding = tf.layers.flatten(embedding)
-            embedding_list.append(embedding)
-        embedding = tf.concat(embedding_list, 1)
-        return embedding
-
-    def process(self, feature_dict, labels):
-        embedding = self.get_embedding(feature_dict)
-        return embedding, labels
+def init_variables(config):
+    variable_dict = dict()
+    for name in config.keys():
+        params = config[name]
+        variable = tf.get_variable(name, shape=[params['buckets'], params['dim']], dtype=tf.float32,
+                                   initializer=tf.truncated_normal_initializer(stddev=0.002))
+        variable_dict[name] = variable
+    return variable_dict
 
 
-def model_fn(features, labels, mode, config):
-    y1 = tf.layers.dense(features, 128, activation=tf.nn.relu)
-    y2 = tf.layers.dense(y1, 1, activation=tf.nn.sigmoid)
+def model_fn_builder(lr, layer_fn):
+    def model_fn(features, labels, mode, config):
+        embedding = layer_fn(features)
+        logits = tf.layers.dense(embedding, 1, activation=tf.nn.sigmoid)
 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        predicts = {'predicts': y2}
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predicts)
+        is_predict = (mode == tf.estimator.ModeKeys.PREDICT)
+        if not is_predict:
+            loss = tf.losses.mean_squared_error(labels=labels, logits=logits)
+            mse = tf.metrics.mean_squared_error(labels=labels, predictions=logits)
+            metrics = {'mse': mse}
+            if mode == tf.estimator.ModeKeys.EVAL:
+                return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=metric_fn(labels, logits))
 
-    yt = tf.cast(labels, tf.float32)
-    loss = tf.losses.mean_squared_error(yt, y2)
-    mse = tf.metrics.mean_squared_error(yt, y2)
-    metrics = {'mse': mse}
-
-    if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.005)
-    train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, eval_metric_ops=metrics)
+            train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss=loss, global_step=tf.train.get_global_step())
+            return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op, eval_metric_ops=metrics)
+        else:
+            predictions = {'predicts':logits}
+            return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+    return model_fn
 
 
 def regression_test():
-    config = {
-        'age': {'dim': 10, 'buckets': 100, 'type': 'int', 'sequence': 1},
-        'gender': {'dim': 10, 'buckets': 10, 'type': 'string', 'sequence': 1},
-        'occupation': {'dim': 10, 'buckets': 1000, 'type': 'int', 'sequence': 1},
-        'code': {'dim': 10, 'buckets': 10000, 'type': 'string', 'sequence': 1},
-        'genres': {'dim': 10, 'buckets': 1000, 'type': 'string', 'sequence': 3},
-    }
-
+    tf.logging.set_verbosity(tf.logging.INFO)
+    feature_columns = [
+        {'feature_name':'age', 'embedding_dim': 10, 'hash_buckets': 100, 'type': 'int', 'sequence': 1},
+        {'feature_name':'gender','embedding_dim': 10, 'hash_buckets': 10, 'type': 'string', 'sequence': 1},
+        {'feature_name':'occupation','embedding_dim': 10, 'hash_buckets': 1000, 'type': 'int', 'sequence': 1},
+        {'feature_name':'code', 'embedding_dim': 10, 'hash_buckets': 10000, 'type': 'string', 'sequence': 1},
+        {'feature_name':'genres', 'embedding_dim': 10, 'hash_buckets': 1000, 'type': 'string', 'sequence': 3},
+    ]
+    hidden_units = [32, 64]
+    layer_fn = build_layers(feature_columns, hidden_units)
     datas = RatingDataset()
     sample_dict, labels = datas.get_samples()
+    model_fn = model_fn_builder(0.001, layer_fn)
+
+    config = tf.estimator.RunConfig(save_checkpoints_steps=100)
+    model = tf.estimator.Estimator(model_fn=model_fn, model_dir=os.path.join(output_path, 'regressor'), config=config)
 
     def input_fn():
         dataset = tf.data.Dataset.from_tensor_slices((sample_dict, labels))
-        dataset.map(net.process)
-        dataset = dataset.shuffle(buffer_size=1000).batch(128, drop_remainder=False)
-        iterator = dataset.make_one_shot_iterator()
-        return iterator.get_next()
+        dataset = dataset.shuffle(1000).repeat(1).batch(128)
+        # iterator = dataset.make_one_shot_iterator()
+        # return iterator.get_next()
+        return dataset
 
-    net = RegressorNet(config)
-    model = tf.estimator.Estimator(model_fn, model_dir=os.path.join(output_path, 'estimator'))
-    model.train(input_fn)
+    model.train(input_fn=input_fn)
 
 
 
---
+++ tensorflow/recommendation/layers.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+
+
+def build_layers(feature_columns, hidden_units):
+    variable_dict = dict()
+    for param in feature_columns:
+        name = param['feature_name']
+        dim = param['embedding_dim']
+        buckets = param['hash_buckets']
+        variable = tf.get_variable(name=name, shape=[buckets, dim], dtype=tf.float32,
+                                   initializer=tf.truncated_normal_initializer(stddev=0.02))
+        variable_dict[name] = variable
+        
+    def layer_fn(features):
+        embedding_list = list()
+        for param in feature_columns:
+            name = param['feature_name']
+            buckets = param['hash_buckets']
+            dim = param['embedding_dim']
+            seq = param['sequence']
+            feature_type = param['type']
+            value = features[name]
+
+            if feature_type == 'string':
+                indices = tf.strings.to_hash_bucket_fast(value, buckets)
+                embedding = tf.nn.embedding_lookup(variable_dict[name], indices)
+            elif feature_type == 'int':
+                embedding = tf.nn.embedding_lookup(variable_dict[name], value)
+            else:
+                continue
+
+            embedding = tf.layers.flatten(embedding)
+            for unit in hidden_units:
+                embedding = tf.layers.dense(embedding, unit, activation=tf.nn.relu)
+            embedding = tf.layers.dense(embedding, dim, activation=tf.nn.tanh)
+            embedding_list.append(embedding)
+        ret = tf.concat(embedding, axis=1)
+        ret = tf.layers.dense(ret, 32, activation=tf.nn.tanh)
+        ret = tf.nn.l2_normalize(ret)
+        return ret
+
+    return layer_fn
+
+
+
+            
+        
+        
