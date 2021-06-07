74e1b8b8ddcc3c8525f9feb0bcbd1056a353372c
chenx <chenx@chenxdeMacBook-Pro.local>
2021-06-07 23:56:28 +0800

    modified

--- pytorch/test/tensorboardx.py
+++ pytorch/test/tensorboardx.py
@@ -6,8 +6,10 @@ from torchvision import models, transforms, datasets
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
 
+import numpy as np
 
-data_root = '/Users/chenxiang/Downloads/Gitlab/Deepmind/datas'
+
+data_root = '/Users/chenx/Desktop/study/data'
 
 class TestCNN(nn.Module):
     def __init__(self):
@@ -18,6 +20,7 @@ class TestCNN(nn.Module):
             nn.ReLU(inplace=True)
         )
 
+
     def forward(self, x):
         return self.conv(x)
 
@@ -25,21 +28,33 @@ class TestCNN(nn.Module):
 
 def main():
     image_transform = transforms.ToTensor()
-    # datas = datasets.FashionMNIST(root='../datas', train=True, download=True, transform=image_transform)
     datas = datasets.CIFAR10(root=data_root, train=True, download=True, transform=image_transform)
     data_loader = DataLoader(datas, batch_size=100, shuffle=True)
     tb = SummaryWriter()
     net = TestCNN()
 
     for index, (data, label) in enumerate(data_loader):
-        grid = make_grid(data)
+        grid = make_grid(data, nrow=10)
         tb.add_image("images", grid)
-        tb.add_graph(net, data)
+        # tb.add_graph(net, data)
         break
 
     tb.close()
 
 
+def summary_test():
+    writer = SummaryWriter(comment='wave')
+    epochs = 100
+    for epoch in range(epochs):
+        x = epoch
+        writer.add_scalar('data/scalar', x * np.sin(x), x)
+        # writer.add_scalar('data/scalar', x * np.cos(x), x)
+
+    writer.close()
+
+
 
 if __name__ == '__main__':
-    main()
+    # main()
+
+    summary_test()
