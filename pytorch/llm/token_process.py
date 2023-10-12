13b2e74559aeb35b745ef397512179afe338479a
ckirchhoff <515629648@qq.com>
2023-10-12 16:58:27 +0800

    Update token_process.py

--- pytorch/llm/token_process.py
+++ pytorch/llm/token_process.py
@@ -97,6 +97,71 @@ def trainer_test():
     print(type(predicts.label_ids))
 
 
+def full_train():
+    tokenizer = transformers.AutoTokenizer.from_pretrained(bert_cased_path)
+    raw_datasets = load_dataset(data_path, "mrpc")
+    def tokenize_function(example):
+        return tokenizer(example["text1"], example["text2"], truncation=True)
+
+    tokenized_dataset = raw_datasets.map(tokenize_function, remove_columns=['text1', "text2", "idx", "label_text"], batched=True)
+    # tokenized_dataset.remove_columns(['text1', "text2", "idx","label_text"])  *** useless ***
+    tokenized_dataset.rename_column("label", "labels")
+    tokenized_dataset.set_format("torch")
+    print(tokenized_dataset['train'].column_names)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    train_dataloader = DataLoader(tokenized_dataset['train'], shuffle=True, batch_size=8, collate_fn=data_collator)
+    eval_dataloader = DataLoader(tokenized_dataset['validation'], shuffle=True, batch_size=8, collate_fn=data_collator)
+    for batch in train_dataloader:
+        print({k: v.shape for k,v in batch.items()})
+        break
+    model = AutoModelForSequenceClassification.from_pretrained(bert_cased_path, num_labels=2)
+    outputs = model(**batch)
+    print(outputs.loss, outputs.logits.shape)
+
+    # device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    # model.to(device)
+    optimizer = AdamW(model.parameters(), lr=5e-5)
+
+    accelerator = Accelerator()
+    train_dataloader, eval_dataloader, model, optimizer = accelerator.prepare(train_dataloader, eval_dataloader, model,
+                                                                              optimizer)
+    num_epochs = 10
+    num_training_steps = num_epochs * len(train_dataloader)
+    lr_scheduler = get_scheduler(
+        "linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps,
+    )
+    progress_bar = tqdm(range(num_training_steps))
+    metric = evaluate.load("../../evaluate-main/metrics/accuracy")
+    model.train()
+    for epoch in range(num_epochs):
+        for batch in train_dataloader:
+            # batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+            loss = outputs.loss
+            # loss.backward()
+            accelerator.backward(loss)
+
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            progress_bar.update(1)
+
+    model.eval()
+    for batch in eval_dataloader:
+        # batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+        logits = outputs.logits
+        predictions = torch.argmax(logits, dim=-1)
+        metric.add_batch(predictions=predictions, references=batch["labels"])
+    metrics = metric.compute()
+    print(metrics)
+
+
 if __name__ == '__main__':
     # tokenizer_test()
     mrpc_test()
