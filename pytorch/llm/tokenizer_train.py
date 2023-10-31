40a2f1434f3391dffc96652e559c153f943b8689
ckirchhoff <515629648@qq.com>
2023-10-31 20:14:38 +0800

    Update tokenizer_train.py

--- pytorch/llm/tokenizer_train.py
+++ pytorch/llm/tokenizer_train.py
@@ -40,5 +40,114 @@ def train_tokenizer():
     tokenizer.save_pretrained("/home/cx/output/llm/code-search-net-tokenizer")
 
 
+
+def test_tokenizer():
+    tokenizer = transformers.AutoTokenizer.from_pretrained("/home/cx/checkpoints/bert-base")
+    example = "My name is Sylvain and I work at Hugging Face in Brooklyn."
+    encoding = tokenizer(example)
+    print(type(encoding))
+    print(tokenizer.is_fast)
+    print(encoding.tokens())
+    print(encoding.word_ids())
+    start, end = encoding.word_to_chars(1)
+    print(start, end, example[start:end])
+
+    inputs_with_offsets = tokenizer(example, return_offsets_mapping=True)
+    print(inputs_with_offsets["offset_mapping"])
+
+    from transformers import pipeline
+    ckpt_path = "/home/cx/checkpoints/bert-large-cased-finetuned-con1103-english"
+    token_classifier = pipeline("token-classification",
+                                model=ckpt_path,
+                                aggregation_strategy="simple")
+    example = "My name is Sylvain and I work at Hugging Face in Brooklyn."
+    ret = token_classifier(example)
+    print(ret)
+
+    import torch
+    from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+    tokenizer = AutoTokenizer.from_pretrained(ckpt_path)
+    model = AutoModelForTokenClassification.from_pretrained(ckpt_path)
+    inputs = tokenizer(example, return_tensors="pt")
+    outputs = model(**inputs)
+    print(inputs["input_ids"].shape)
+    print(outputs.logits.shape)
+
+    probabilities = torch.nn.functional.softmax(outputs.logits, dim=-1)[0].tolist()
+    predictions = outputs.logits.argmax(dim=-1)[0].tolist()
+    print(probabilities)
+    print(predictions)
+    print(model.config.id2label)
+
+    results = []
+    tokens = inputs.tokens()
+    for idx, pred in enumerate(predictions):
+        label = model.config.id2label[pred]
+        if label != "O":
+            results.append(
+                {"entity": label, "score": probabilities[idx][pred], "word": tokens[idx]}
+            )
+    print(results)
+    inputs_with_offsets = tokenizer(example, return_offsets_mapping=True)
+    print(inputs_with_offsets["offset_mapping"])
+
+    results = []
+    inputs_with_offsets = tokenizer(example, return_offsets_mapping=True)
+    tokens = inputs_with_offsets.tokens()
+    offsets = inputs_with_offsets["offset_mapping"]
+    for idx, pred in enumerate(predictions):
+        label = model.config.id2label[pred]
+        if label != "O":
+            start, end = offsets[idx]
+            results.append(
+                {
+                    "entity": label,
+                    "score": probabilities[idx][pred],
+                    "word": tokens[idx],
+                    "start": start,
+                    "end": end,
+                }
+            )
+    print(results)
+
+    import numpy as np
+    results = []
+    inputs_with_offsets = tokenizer(example, return_offsets_mapping=True)
+    tokens = inputs_with_offsets.tokens()
+    offsets = inputs_with_offsets["offset_mapping"]
+    idx = 0
+    while idx < len(predictions):
+        pred = predictions[idx]
+        label = model.config.id2label[pred]
+        if label != "O":
+            # Remove the B- or I-
+            label = label[2:]
+            start, _ = offsets[idx]
+            # Grab all the tokens labeled with I-label
+            all_scores = []
+            while (
+                    idx < len(predictions)
+                    and model.config.id2label[predictions[idx]] == f"I-{label}"
+            ):
+                all_scores.append(probabilities[idx][pred])
+                _, end = offsets[idx]
+                idx += 1
+            # The score is the mean of all the scores of the tokens in that grouped entity
+            score = np.mean(all_scores).item()
+            word = example[start:end]
+            results.append(
+                {
+                    "entity_group": label,
+                    "score": score,
+                    "word": word,
+                    "start": start,
+                    "end": end,
+                }
+            )
+        idx += 1
+    print(results)
+
+
 if __name__ == '__main__':
     train_tokenizer()
